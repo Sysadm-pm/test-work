cf09c4fef0aca847a6675ba17c13e04e83278fe7
pmalyovanyi <sysadm@fex.net>
2018-07-26 10:10:32 +0300

    260718 10:10

--- test.sql
+++ test.sql
@@ -20,22 +20,23 @@ USE `test`;
 CREATE TABLE IF NOT EXISTS `goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `art` int(11) DEFAULT NULL,
-  `title` char(50) DEFAULT NULL,
-  `description` varchar(50) DEFAULT NULL,
+  `title` char(200) DEFAULT NULL,
+  `description` varchar(5000) DEFAULT NULL,
   `category` char(50) DEFAULT NULL,
-  `tags` char(50) DEFAULT NULL,
+  `tags` char(200) DEFAULT NULL,
   `mainPrice` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Dumping data for table test.goods: ~5 rows (приблизно)
+-- Dumping data for table test.goods: ~6 rows (приблизно)
 /*!40000 ALTER TABLE `goods` DISABLE KEYS */;
 REPLACE INTO `goods` (`id`, `art`, `title`, `description`, `category`, `tags`, `mainPrice`) VALUES
 	(1, 0, 'New test goods\r\n', 'Some kind of goods', 'main', 'main,wool,cool', 10),
 	(2, 0, 'New test goods\r\n', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2000),
-	(3, 0, 'New test goods\r\n', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2000),
-	(4, 0, 'New test goods\r\n', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2000),
-	(5, 0, 'New test goods\r\n', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2000);
+	(3, 2201, 'New test goods 1', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2001),
+	(4, 2202, 'New test goods 2', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2002),
+	(5, 2203, 'New test goods 3', 'Some kind of goods', 'electronic', 'electronic,smartphone,2sim', 2003),
+	(6, 2202, 'Школьная форма', '<p><br></p><p>В «NUI VERY» круглый год доступна&nbsp;большая коллекция&nbsp;школьной формы, \r\nкоторая шьется исключительно из высококачественной&nbsp;ткани. Состав ткани: \r\nшерсть 30%,полиэстер40% , хлопок 30%. Представленные модели школьной \r\nформы удовлетворят пожелания и маленьких&nbsp;модников-первоклашек и стильных\r\n подростков-старшеклассников.</p><p>\r\n\r\n<br></p><p align="center"><strong>Уважаемые родители!</strong></p><p>\r\n\r\n<br></p><p>Наше предприятие, при изготовлении школьной формы для наших детей, \r\nпридерживается предусмотренным действующим в Украине нормам и \r\nстандартам, используя высококачественный материал, который закупается у \r\nиностранных производителей.</p><p>\r\n\r\n<br></p><p>В связи с тем, что закупка тканей осуществляется у иностранных \r\nпроизводителей, цена изделия непосредственно зависит от курса гривны к \r\nдоллару США.</p><p>\r\n\r\n<br></p><p>Учитывая нестабильную экономическую ситуацию в стране и, как \r\nследствие, повышения курса доллара США, возможны увеличение цены на \r\nизделия.</p><p>\r\n\r\n<br></p><p>Убедительная просьба, отнестись к возможным изменениям цен с пониманием.</p><p>\r\n\r\n<br></p><p>За ранее благодарим Вас за понимание и сотрудничество.</p><p>\r\n\r\n<br></p><p>С уважением, администрация.</p><p><br></p>', 'Школьная форма', 'Школьная форма,одежда', 2005);
 /*!40000 ALTER TABLE `goods` ENABLE KEYS */;
 
 -- Dumping structure for таблиця test.priceperiod
@@ -49,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `priceperiod` (
   PRIMARY KEY (`ppid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
--- Dumping data for table test.priceperiod: ~10 rows (приблизно)
+-- Dumping data for table test.priceperiod: ~9 rows (приблизно)
 /*!40000 ALTER TABLE `priceperiod` DISABLE KEYS */;
 REPLACE INTO `priceperiod` (`ppid`, `gid`, `startDate`, `endDate`, `diffDate`, `price`) VALUES
 	(11, 1, '2018-06-25 00:00:00', '2018-07-25 00:00:00', 2592000, 100000),
